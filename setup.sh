c78fe6a514092b266b250ba953c38056a40b77ee
Simon Forteza <fortezasimon@gmail.com>
2025-03-08 21:24:18 -0300

    ADD - script de setup para windows terminado?

--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Verifica si el archivo .config_done existe
+if [ -f ".config_done" ]; then
+    echo "La configuración ya ha sido completada previamente."
+    echo "Presione cualquier tecla para salir..."
+    read -n 1 -s
+    exit 0
+fi
+
+echo "Iniciando la instalación..."
+
 # Verifica si Python está instalado
-if ! command -v python3 &> /dev/null; then
-    echo "Python3 no está instalado. Por favor instálalo antes de continuar."
+if ! command -v python &> /dev/null; then
+    echo "Python no está instalado. Por favor instálalo antes de continuar."
     exit 1
 fi
 
 # Crea el entorno virtual si no existe
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    python -m venv venv
+    venv/bin/python -m pip install --upgrade pip
     echo "Entorno virtual creado exitosamente."
 fi
 
@@ -24,31 +35,29 @@ else
     exit 1
 fi
 
-
 # Solicita el email dos veces y verifica que coincidan
 while true; do
     read -p "Ingrese su correo electrónico: " email1
     read -p "Confirme su correo electrónico: " email2
 
-    # Verificación de formato básico de email
-    if [[ ! "$email1" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-        echo "Correo electrónico no válido. Inténtelo nuevamente."
-        continue
-    fi
-
-    if [ "$email1" == "$email2" ]; then
-        echo "Correo electrónico verificado correctamente."
-        break
+    # Verificación simple de email (contiene @ y .)
+    if [[ "$email1" =~ @.*\..* ]]; then
+        if [ "$email1" == "$email2" ]; then
+            echo "Correo electrónico verificado correctamente."
+            break
+        else
+            echo "Los correos electrónicos no coinciden. Inténtelo de nuevo."
+        fi
     else
-        echo "Los correos electrónicos no coinciden. Inténtelo de nuevo."
+        echo "Correo electrónico no válido. Intentelo nuevamente."
     fi
 done
 
 # Solicita la contraseña dos veces y verifica que coincidan
 while true; do
-    read -s -p "Ingrese su contraseña: " password1
+    read -sp "Ingrese su contraseña: " password1
     echo
-    read -s -p "Confirme su contraseña: " password2
+    read -sp "Confirme su contraseña: " password2
     echo
 
     if [ "$password1" == "$password2" ]; then
@@ -60,14 +69,12 @@ while true; do
 done
 
 # Guarda el email y la contraseña en el archivo .env
-cat <<EOF > .env
-EMAIL=$email1
-PASSWORD=$password1
-EOF
+echo "MY_USERNAME=$email1" > .env
+echo "PASSWORD=$password1" >> .env
 
-
-#Ejecuta el script para obtener la url
-PYTHONPATH=$(pwd)/src python3 src/main.py
+# Ejecuta el script para obtener la URL
+export PYTHONPATH=$(pwd)/src
+python src/get_url.py
 
 # Crear el archivo de control
 touch .config_done
